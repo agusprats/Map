094ea6f9d065400a61a7e690bd63ee37617ac999
Agustina Prats <agusprats@gmail.com>
2021-02-23 16:05:04 -0300

    v.3

--- src/adaitw/java/Main.java
+++ src/adaitw/java/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         Map<Integer, Persona> mapaPersonas = new HashMap<>();
+//O HashMap<Integer, Persona> mapaPersonas = new HashMap<>();
 
         Persona p1 = new Persona(123123, "Pablo", "abcabc", 23);
         Persona p2 = new Persona(223344, "Teresa", "ffff", 43);
@@ -22,6 +23,7 @@ public class Main {
         mapaPersonas.put(123123, new Persona(156, "otro nombre", "otra direccion", 33 ));
         Persona nuevaP = mapaPersonas.get(123123);
        // mapaPersonas.remove(123123);
+        mapaPersonas.put(nuevaP.getDni(), nuevaP);
 
         System.out.println("Nueva persona: "+nuevaP);
 
